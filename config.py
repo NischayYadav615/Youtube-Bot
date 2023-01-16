23d87059ade89bde28a3557b3ffedd353a4f3bf5
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-01-16 12:48:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import  os
 
-TOKEN = os.environ.get("TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-CHAT_ID = os.environ.get("CHAT_ID")
+TOKEN = os.environ.get("5965806465:AAEXSWoCdu5kCYljILVVkrEhOXsnvTDYEfE")
+APP_ID = int(os.environ.get("19911978"))
+API_HASH = os.environ.get("e3f5848d4c384af9e6f1f52ca84c19c7")
+CHAT_ID = os.environ.get("5826950002")
 
 
-youtube_next_fetch = 0  # time in minute
+youtube_next_fetch = 0  # time in minutep
 
 
 EDIT_TIME = 5
